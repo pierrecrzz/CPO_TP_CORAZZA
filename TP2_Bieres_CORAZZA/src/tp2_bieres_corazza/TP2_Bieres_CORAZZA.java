b3b63b5ad316c87608d97f0e36a78c10e0aef5d2
coraz <coraz@LAPTOP-0SM82EUU.education.lakanal.epf.fr>
2022-10-25 15:33:19 +0200

    Création des objets bieres et initialisation

--- TP2_Bieres_CORAZZA/src/tp2_bieres_corazza/TP2_Bieres_CORAZZA.java
+++ TP2_Bieres_CORAZZA/src/tp2_bieres_corazza/TP2_Bieres_CORAZZA.java
@@ -14,6 +14,10 @@ public class TP2_Bieres_CORAZZA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        BouteilleBiere uneBiere = new BouteilleBiere();
+        uneBiere.nom = "Cuvée des trolls";
+        uneBiere.degreAlcool = 7.0 ;
+        
         // TODO code application logic here
     }
     
