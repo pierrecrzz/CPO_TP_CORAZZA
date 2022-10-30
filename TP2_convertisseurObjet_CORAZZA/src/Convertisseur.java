0a878ca6d4990a02224aae53d0b3f370ce72cccd
coraz <coraz@LAPTOP-0SM82EUU.education.lakanal.epf.fr>
2022-10-30 22:42:45 +0100

    Conversion de toutes les unités en une autre

--- TP2_convertisseurObjet_CORAZZA/src/Convertisseur.java
+++ TP2_convertisseurObjet_CORAZZA/src/Convertisseur.java
@@ -1,6 +1,3 @@
-
-import java.util.Scanner;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -10,17 +7,64 @@ import java.util.Scanner;
  *
  * @author coraz
  */
-public class Convertisseur  {
+public class Convertisseur {
     int nbConversions;
     
-    public Convertisseur() {
-        nbConversions = 0;
-    }
+
+public Convertisseur()  { 
+    nbConversions = 0 ;               
+}
+
+@Override
+public String toString () {
+
+return "nombre de conversion est "+ nbConversions;
+}
+
+
+public double CelciusVersKelvin (double tempCelcius) {
     
-    
-    @Override
-    public String toString(){       
-        return "nombre de conversions : "+ nbConversions;
-    }
-     
+    double res = (double) (tempCelcius + 273.15) ;
+    nbConversions = nbConversions + 1;
+    return res  ;
+}
+
+
+public double KelvinVersCelcius (double tempKelvin)  {
+
+    double res=( double ) (tempKelvin - 273.15 )  ;
+    nbConversions = nbConversions + 1 ;
+    return res ;
+}
+
+
+public double FarenheitVersCelcius (double tempFahren)  {
+
+    double res=( double ) (tempFahren - 32)/1.8  ;
+    nbConversions = nbConversions + 1;
+    return res  ;
+}
+
+
+public double CelciusVersFarenheit (double tempCelcius)  {
+
+    double res=(double) (tempCelcius + 32 )*1.8 ;
+    nbConversions = nbConversions + 1 ;
+    return res  ;
+}
+
+
+public double KelvinVersFarenheit (double tempKelvin)  {
+    double tempCelsius=KelvinVersCelcius( tempKelvin );
+    double res = CelciusVersFarenheit( tempCelsius );
+    nbConversions = nbConversions + 1;
+    return res  ;
+}
+
+public double FarenheitVersKelvin (double tempFahren)   {
+    double tempCelcius = FarenheitVersCelcius (tempFahren) ;
+    double res=CelciusVersKelvin(tempCelcius) ;
+    nbConversions = nbConversions + 1  ;
+    return res  ;
+}    
 }
