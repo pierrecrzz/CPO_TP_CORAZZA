d3067b92951fc2654630c10cb782a06073899851
coraz <coraz@LAPTOP-0SM82EUU.education.lakanal.epf.fr>
2022-10-30 22:05:19 +0100

    Création et affichage de deux nouvelles bières

--- TP2_Bieres_CORAZZA/src/tp2_bieres_corazza/TP2_Bieres_CORAZZA.java
+++ TP2_Bieres_CORAZZA/src/tp2_bieres_corazza/TP2_Bieres_CORAZZA.java
@@ -13,12 +13,20 @@ public class TP2_Bieres_CORAZZA {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        BouteilleBiere uneBiere = new BouteilleBiere();
-        uneBiere.nom = "Cuvée des trolls";
+    public static void main(String[] args)  {
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0 ,"Dubuisson") ;
+        uneBiere.Nom = "Cuvée des trolls" ;
         uneBiere.degreAlcool = 7.0 ;
+        String brasserie= "Dubuisson" ;
+        boolean ouverte= false ;
+        uneBiere.lireEtiquette() ;
+        System.out.println(uneBiere) ; 
         
-        // TODO code application logic here
-    }
-    
+        
+        BouteilleBiere biere1 = new BouteilleBiere ("Bière de bonhomme", 75.2, "BaobabDuSud") ;
+        System.out.println(biere1) ; 
+        BouteilleBiere biere2 = new BouteilleBiere ("Bière de Goblin", 74.4, "Tranchéecoupante") ;
+        System.out.println(biere2) ; 
+}
+
 }
